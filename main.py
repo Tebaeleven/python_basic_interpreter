6bd5471f31582ed6abdd92ab1c37434126b69c5e
nano <nano@nanonoMacBook-Air.local>
2023-01-06 01:53:41 +0900

    状態遷移図を使ってプログラムを改良する

--- main.py
+++ main.py
@@ -2,28 +2,37 @@ def main():
     """
     整数1 {+|-|*|/} 整数2
     """
-
+    state=0
     while True:
-        # 整数1の入力
         st=input(">")
         if st.upper() == "QUIT":
             break
-        a=int(st)
-        # 演算子の入力
-        st=input(">")
-        op=st
-        # 整数2の入力
-        st=input(">")
-        b=int(st)
-        # 計算して表示
-        if op== "+":
-            print (a+b)
-        elif op =="-":
-            print(a-b)
-        elif op =="*":
-            print(a*b)
-        elif op =="/":
-            print(a/b)
+
+        if state == 0:
+            # 整数1の入力
+            a=int(st)
+            state=1
+            continue
+        if state == 1:
+            op=st
+            state=2
+            continue
+        if state == 2:
+            # 整数2の入力
+            b=int(st)
+            state=0
+            # 計算して表示
+            if op== "+":
+                print (a+b)
+            elif op =="-":
+                print(a-b)
+            elif op =="*":
+                print(a*b)
+            elif op =="/":
+                print(a/b)
+            continue
+
+
 
 if __name__ == '__main__':
     main()
