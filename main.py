ccebba79a842c33509ba8eefc9a4ea4284420f40
nano <nano@nanonoMacBook-Air.local>
2023-01-06 02:32:03 +0900

    数字のチェックルーチンを自作

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 def main():
     """
-    整数1 {+|-|*|/} 整数2
+    数字1 {+|-|*|/} 数字2
     """
     state=0
     while True:
         st=input(">")
         if st.upper() == "QUIT":
             break
-
         if state == 0:
-            # 整数1の入力
-            if not st.isdigit():
+            # 数字1の入力
+            if not isNumeric(st):
                 print("数字ではありません")
                 continue
-            a=int(st)
+            a=float(st)
             state=1
             continue
         if state == 1:
@@ -25,11 +24,11 @@ def main():
             state=2
             continue
         if state == 2:
-            # 整数2の入力
-            if not st.isdigit():
+            # 数字2の入力
+            if not isNumeric(st):
                 print("数字ではありません")
                 continue
-            b=int(st)
+            b=float(st)
             state=0
             # 計算して表示
             if op== "+":
@@ -42,6 +41,64 @@ def main():
                 print(a/b)
             continue
 
+
+def isNumeric(st):
+    """
+    数字（少数含む）かどうかのチェックルーチン
+        [+|-]{{0-9}・・・|[0-9]・・・ .[0-9]・・・}
+    """
+    st +="\n"
+    pos=0
+    # 小数点があるか
+    decimal_point=False
+    # 数字
+    integer=0
+    # 小数
+    decimal=0
+    state=0
+    while True:
+        # +,-判定
+        ch = st[pos]
+        if state==0:
+            if ch in "+-":
+                state=1
+                pos=pos+1
+                continue
+            state=1
+            continue
+        # 整数、小数判定
+        if state ==1:
+            if "0" <= ch <= "9":
+                integer+=1
+                state=1
+                pos+=1
+                continue
+            if ch == ".":
+                decimal_point=True
+                state=2
+                pos+=1
+                continue
+            state=100
+            continue
+        # 小数の後の整数判定
+        if state==2:
+            if "0" <= ch <= "9":
+                decimal+=1
+                state=2
+                pos+=1
+                continue
+            state=100
+            continue
+        if state==100:
+            # 小数点があるのに小数がない場合
+            if decimal_point and decimal==0:
+                return False
+            # 小数点がないのに数字が0個の場合
+            if not decimal_point and integer==0:
+                return False
+            if ch=="\n":
+                return True
+            return False
 
 
 if __name__ == '__main__':
