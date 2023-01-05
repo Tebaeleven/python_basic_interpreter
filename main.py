e21f82ccdc45d85e471849def21e6210f3deb490
nano <nano@nanonoMacBook-Air.local>
2023-01-06 02:00:24 +0900

    エラーチェックの追加

--- main.py
+++ main.py
@@ -10,15 +10,25 @@ def main():
 
         if state == 0:
             # 整数1の入力
+            if not st.isdigit():
+                print("数字ではありません")
+                continue
             a=int(st)
             state=1
             continue
         if state == 1:
+            # 演算子の入力
+            if len(st)!=1 or st not in "+-*/":
+                print("演算子(+,-,*,/)ではない")
+                continue
             op=st
             state=2
             continue
         if state == 2:
             # 整数2の入力
+            if not st.isdigit():
+                print("数字ではありません")
+                continue
             b=int(st)
             state=0
             # 計算して表示
