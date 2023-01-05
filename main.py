2b748d1de146159c8e01a610d814f0014a0b461a
nano <nano@nanonoMacBook-Air.local>
2023-01-06 03:47:35 +0900

    演算子に優先順位をつける

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+class Stack:
+    """スタック"""
+    def __init__(self):
+        self.buff=[]
+
+    def push(self,st):
+        self.buff.insert(0,st)
+    
+    def pop(self):
+        if len(self.buff)==0:
+            print("stack is empty")
+            return None
+        return self.buff.pop(0)
+
+    def size(self):
+            return len(self.buff)
+
+    def print(self):
+            print(self.buff)
+
 def main():
     """
     数字1 {{+|-|*|/} 数字2 | = }・・・
     0     1        2     1   1
     """
+    stk=Stack()
     state=0
     while True:
+        stk.print()
+        #print("stateの数"+str(state))
         st=input(">")
         if st.upper() == "QUIT":
             break
@@ -13,19 +36,45 @@ def main():
             if not isNumeric(st):
                 print("数字ではありません")
                 continue
-            a=float(st)
+            stk.push(float(st))
             state=1
             continue
         if state == 1:
             # 演算子の入力
             if st=="=":
+                if stk.size()==1:
+                    a=stk.pop()
+                else:
+                    b=stk.pop()
+                    op=stk.pop()
+                    a=stk.pop()
+                    if op=="+":
+                        a+=b
+                    else:
+                        a-=b
                 print(a)
+                stk.push(a)
                 state=1
                 continue
             if len(st)!=1 or st not in "+-*/":
                 print("演算子(+,-,*,/)ではない")
                 continue
             op=st
+            if op in "*/":
+                stk.push(st)
+            else: # +,-
+                if stk.size()==3:
+                    b=stk.pop()
+                    op1=stk.pop()
+                    a=stk.pop()
+                    if op1=="+":
+                        a+=b
+                    else:
+                        a-=b
+                    stk.push(a)
+                    stk.push(op)
+                else:
+                    stk.push(op)
             state=2
             continue
         if state == 2:
@@ -34,17 +83,19 @@ def main():
                 print("数字ではありません")
                 continue
             b=float(st)
+            op=stk.pop()
+            a=stk.pop()
+            if op in "*/":
+                if op=="*":
+                    a*=b
+                else:
+                    a/=b
+                stk.push(a)
+            else:
+                stk.push(a)
+                stk.push(op)
+                stk.push(b)
             state=1
-            # 計算して表示
-            if op== "+":
-                a+=b
-            elif op =="-":
-                a-=b
-            elif op =="*":
-                a*=b
-            elif op =="/":
-                a/=b
-            # print(a)
             continue
 
 
